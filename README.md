454ede4a8933fad8121b736ce4accece79f74f41
Athan Clark <athan.clark@gmail.com>
2016-05-12 16:01:30 -0600

    data communication

---
+++ README.md
@@ -0,0 +1,76 @@
+# Elm-Debouncer
+
+HEED ALL YEE WHO HATH WANT TO DEBOUNCE STUFF, because it's chill and we
+all know what happens to chill people >_>
+
+Basically the idea is simple, a `Debouncer` is a stateful component
+that manages _when_ an arbitrary action of your own should be issued.
+The `Deboucer` itself is basically the model for the delay, so you
+should have a separate one for different isolated miniscule components
+of your applicaiton - like one for each text box.
+
+To let the debouncer actually issue your actions for you, you need to
+delegate that work to the updating function - `updateDebouncer`. Note
+that it needs the delay and action to issue as read-only data - we won't
+be changing the delay or action to issue dynamically here.
+
+When viewing this system correctly, we should consider the debouncer somewhat
+like a _service_ - when we issue the `Start` action / message, we're actually
+asking the debouncer to issue the main action. from this perspective,
+`DebouncerMsg` (with `Start` as the only exposed constructor) as our input interface -
+the interface we can solicit with queries, while `DebouncerResults` is our output
+interface - representing either more debouncing or an issuance of the main
+action we wish to debounce. Luckily this output channel is very easy to handle,
+using `handleDebouncerResults` all we need is something that manages our inputs,
+so to say that we understand how to handle a `DebouncerMsg` means we can handle
+a `DebouncerResults`. Sorry :\ I'll explain more in a second.
+
+Okay, so here's an example:
+
+```elm
+type alias MyModel =
+  { somethingDank : Dank
+  , myDebouncer   : Debouncer
+  }
+
+initMyModel : MyModel
+initMyModel =
+  { somethingDank = ayyLmao
+  , myDebouncer = initDebouncer
+  }
+  
+type MyAction
+  = DankAction DankAction
+  | DebouncerMsg DebouncerMsg
+  
+updateMyModel : MyAction
+             -> MyModel
+             -> (MyModel, Cmd MyAction)
+updateMyModel action model =
+  case action of
+    DankAction a ->
+      let (awwYiss, eff) = updateDank a model.somethingDank
+      in  ( { model | somethingDank = awwYiss }
+          , Cmd.map DankAction eff
+          )
+    DebouncerMsg a ->
+      let (newDebouncer, eff) = updateDebouncer
+                                  (500 * millisecond)
+                                  (DankAction SomeDankAction)
+                                  a
+                                  model.myDebouncer
+      in  ( { model | myDebouncer = newDebouncer }
+          , Cmd.map (handleDebouncerResults DebouncerMsg) eff
+          )
+          
+viewMyModel : MyModel
+           -> Html MyAction
+viewMyModel model =
+  div []
+    [ h2 [] [text "Dank, right?"]
+    , input [ type' "text"
+            , on "input" (Json.Decode.succeed Start)
+            , value model.somethingDank.dankness
+            ] []
+    ]
+```
--- src/Debouce.elm
+++ src/Debouce.elm
@@ -4,7 +4,7 @@ module Debounce exposing
   , DebouncerResults
   , handleDebouncerResults
   , updateDebouncer
-  , DebouncerMsg (Start)
+  , DebouncerMsg (Bounce)
   )
 
 {-|
@@ -44,35 +44,35 @@ import Process
 
 {-| The state of the debouncer
 -}
-type alias Debouncer =
-  { since : Maybe Time
+type alias Debouncer b =
+  { since : Maybe (Time, b)
   }
 
 {-| The initial debouncer
 -}
-initDebouncer : Debouncer
+initDebouncer : Debouncer b
 initDebouncer =
   { since = Nothing
   }
 
-{-| To bounce the debouncer, just make multiple calls to `Start`.
+{-| To bounce the debouncer, just make multiple calls to `Bounce`.
 -}
-type DebouncerMsg
-  = Start
-  | AssignTime Time
+type DebouncerMsg b
+  = Bounce b
+  | Assign b Time
   | Finish Time
 
 {-| Representing either more messages needing to be handled in this component,
     or the message you've been trying to debounce.
 -}
-type DebouncerResults a
-  = More DebouncerMsg
+type DebouncerResults b a
+  = More (DebouncerMsg b)
   | Done a
 
 {-| By being able to convert a `DebouncerMsg` to a message we understand, we
     can turn a whole `DebouncerResults` to a message we understand.
 -}
-handleDebouncerResults : (DebouncerMsg -> a) -> DebouncerResults a -> a
+handleDebouncerResults : (DebouncerMsg b -> a) -> DebouncerResults b a -> a
 handleDebouncerResults f m =
   case m of
     More a -> f a
@@ -81,20 +81,20 @@ handleDebouncerResults f m =
 {-| The main logic of the debouncer.
 -}
 updateDebouncer : Time
-               -> a
-               -> DebouncerMsg
-               -> Debouncer
-               -> (Debouncer, Cmd (DebouncerResults a))
+               -> (b -> a)
+               -> DebouncerMsg b
+               -> Debouncer b
+               -> (Debouncer b, Cmd (DebouncerResults b a))
 updateDebouncer delay mainAction action model =
   case action of
-    Start ->
+    Bounce x ->
       case model.since of
         Nothing ->
           ( model
           , Cmd.batch
               [ Task.perform
                   Debug.crash
-                  (\t -> More <| AssignTime t)
+                  (\t -> More <| Assign x t)
                   Time.now
               , Task.perform
                   Debug.crash
@@ -106,28 +106,29 @@ updateDebouncer delay mainAction action model =
           ( model
           , Task.perform
               Debug.crash
-              (\t -> More <| AssignTime t)
+              (\t -> More <| Assign x t)
               Time.now
           )
-    AssignTime current ->
-      ( { model | since = Just current }
+    Assign x current ->
+      ( { model | since = Just (current, x) }
       , Cmd.none
       )
     Finish current ->
       case model.since of
-        Nothing         -> Debug.crash "Somehow got in forbidden state x_x"
-        Just oldCurrent ->
+        Nothing ->
+          Debug.crash "Somehow got in forbidden state x_x"
+        Just (oldCurrent,x) ->
           let elapsed = current - oldCurrent
-          in  if elapsed < delay
-              then ( model
-                   , Task.perform
-                       Debug.crash
-                       (\t -> More <| Finish t)
-                       <| Process.sleep (delay - elapsed) `Task.andThen` \_ -> Time.now
-                   )
-              else ( initDebouncer
-                   , Task.perform
-                       Debug.crash
-                       (\x -> x)
-                       <| Task.succeed <| Done mainAction
-                   )
+          in if elapsed < delay
+          then ( model
+               , Task.perform
+                   Debug.crash
+                   (\t -> More <| Finish t)
+                   <| Process.sleep (delay - elapsed) `Task.andThen` \_ -> Time.now
+               )
+          else ( initDebouncer
+               , Task.perform
+                   Debug.crash
+                   (\x -> x)
+                   <| Task.succeed <| Done <| mainAction x
+               )
