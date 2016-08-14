0bde13041feec2fd5f27d6bbff330729a295fac8
Athan Clark <athan.clark@gmail.com>
2016-08-14 09:31:24 -0700

    cont-based

--- src/Debounce.elm
+++ src/Debounce.elm
@@ -31,95 +31,73 @@ _once_, after being given a slew of requests within the delay timeframe.
 -}
 
 import Time exposing (Time)
-import Task
+import Task exposing (Task)
 import Process
 
 
 
-type alias Elapsed b =
+type alias Elapsed a =
   { since : Time
-  , data  : Maybe b
+  , cont  : Cmd a
   }
 
 
 {-| The state of the debouncer
 -}
-type alias Model b =
-  { elapsed : Maybe (Elapsed b)
+type alias Model a =
+  { elapsed : Maybe (Elapsed a)
   }
 
 {-| The initial debouncer
 -}
-init : Model b
+init : Model a
 init =
   { elapsed = Nothing
   }
 
 {-| To bounce the debouncer, just make multiple calls to `Bounce`.
 -}
-type Msg b
-  = Bounce (Maybe b -> Maybe b)
-  | Assign (Maybe b -> Maybe b) Time
+type Msg a
+  = Bounce (Cmd a)
+  | Assign (Cmd a) Time
   | Finish Time
+
+
+performLog : Task e a -> Cmd a
+performLog = Task.perform (Debug.crash << toString) identity
+
+mkCmd : a -> Cmd a
+mkCmd = performLog << Task.succeed
 
 
 {-| The main logic of the debouncer.
 -}
 update : Time
-      -> (Maybe b -> Cmd a)
-      -> Msg b
-      -> Model b
-      -> (Model b, Cmd (Result a (Msg b)))
-update delay mainAction action model =
+      -> Msg a
+      -> Model a
+      -> (Model a, Cmd (Result (Msg a) a))
+update delay action model =
   case action of
     Bounce x ->
       case model.elapsed of
         Nothing ->
-          ( model
-          , Cmd.batch
-              [ Task.perform
-                  Debug.crash
-                  (Ok << Assign x)
-                  Time.now
-              , Task.perform
-                  Debug.crash
-                  (Ok << Finish)
-                  <| Process.sleep delay `Task.andThen` \_ -> Time.now
-              ]
-          )
+          model ! [ Cmd.map (Err << Assign x)
+                 <| performLog Time.now
+                  , Cmd.map (Err << Finish)
+                 <| performLog <| Process.sleep delay `Task.andThen` \_ -> Time.now
+                  ]
         Just _  ->
-          ( model
-          , Task.perform
-              Debug.crash
-              (Ok << Assign x)
-              Time.now
-          )
+          model ! [ Cmd.map (Err << Assign x)
+                 <| performLog Time.now
+                  ]
     Assign f current ->
-      ( { model | elapsed =
-                    case model.elapsed of
-                      Nothing ->
-                        Just { since = current
-                             , data = f Nothing
-                             }
-                      Just elap ->
-                        Just { elap | since = current
-                                    , data  = f elap.data
-                             }
-        }
-      , Cmd.none
-      )
+      { model | elapsed = Just { since = current, cont = f }
+      } ! []
     Finish current ->
       case model.elapsed of
-        Nothing ->
-          ( model
-          , Cmd.none
-          ) -- no-op
+        Nothing -> model ! []
         Just elap ->
           let elapsed = current - elap.since
           in if elapsed < delay
-          then ( model
-               , Cmd.none
-               )
-          else ( init
-               , Cmd.map Err <| mainAction elap.data
-               )
+          then model ! []
+          else init  ! [ Cmd.map Ok elap.cont ]
