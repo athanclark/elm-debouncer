e30f82d09b1f810bb0b9be206411c4719d5a5bad
Niklas Hofer <niklas+dev@lanpartei.de>
2016-12-15 01:18:18 +0100

    elm-format (part of elm-upgrade)

--- src/Debounce.elm
+++ src/Debounce.elm
@@ -1,9 +1,10 @@
-module Debounce exposing
-  ( Model
-  , init
-  , update
-  , Msg (Bounce)
-  )
+module Debounce
+    exposing
+        ( Model
+        , init
+        , update
+        , Msg(Bounce)
+        )
 
 {-|
 
@@ -35,63 +36,80 @@ import Task exposing (Task)
 import Process
 
 
-
 type alias Elapsed a =
-  { since : Time
-  , cont  : Cmd a
-  }
+    { since : Time
+    , cont : Cmd a
+    }
 
 
 {-| The state of the debouncer
 -}
 type alias Model a =
-  { elapsed : Maybe (Elapsed a)
-  }
+    { elapsed : Maybe (Elapsed a)
+    }
+
 
 {-| The initial debouncer
 -}
 init : Model a
 init =
-  { elapsed = Nothing
-  }
+    { elapsed = Nothing
+    }
+
 
 {-| To bounce the debouncer, just make multiple calls to `Bounce`.
 -}
 type Msg a
-  = Bounce (Cmd a)
-  | Assign (Cmd a) Time
-  | Finish Time
+    = Bounce (Cmd a)
+    | Assign (Cmd a) Time
+    | Finish Time
 
 
 performLog : Task e a -> Cmd a
-performLog = Task.perform (Debug.crash << toString) identity
+performLog =
+    Task.perform (Debug.crash << toString) identity
+
 
 mkCmd : a -> Cmd a
-mkCmd = performLog << Task.succeed
+mkCmd =
+    performLog << Task.succeed
 
 
 {-| The main logic of the debouncer.
 -}
-update : Time
-      -> Msg a
-      -> Model a
-      -> (Model a, Cmd (Result (Msg a) a))
+update :
+    Time
+    -> Msg a
+    -> Model a
+    -> ( Model a, Cmd (Result (Msg a) a) )
 update delay action model =
-  case action of
-    Bounce x ->
-        model
-      ! [ Cmd.map (Err << Assign x) <| performLog Time.now
-        , Cmd.map (Err << Finish)
-       <| performLog <| Process.sleep delay `Task.andThen` \_ -> Time.now
-        ]
-    Assign x current ->
-      { model | elapsed = Just { since = current, cont = x }
-      } ! []
-    Finish current ->
-      case model.elapsed of
-        Nothing -> model ! []
-        Just elap ->
-          let elapsed = current - elap.since
-          in if elapsed < delay
-          then model ! []
-          else init  ! [ Cmd.map Ok elap.cont ]
+    case action of
+        Bounce x ->
+            model
+                ! [ Cmd.map (Err << Assign x) <| performLog Time.now
+                  , Cmd.map (Err << Finish) <|
+                        performLog <|
+                            Process.sleep delay
+                                |> Task.andThen (\_ -> Time.now)
+                  ]
+
+        Assign x current ->
+            { model
+                | elapsed = Just { since = current, cont = x }
+            }
+                ! []
+
+        Finish current ->
+            case model.elapsed of
+                Nothing ->
+                    model ! []
+
+                Just elap ->
+                    let
+                        elapsed =
+                            current - elap.since
+                    in
+                        if elapsed < delay then
+                            model ! []
+                        else
+                            init ! [ Cmd.map Ok elap.cont ]
